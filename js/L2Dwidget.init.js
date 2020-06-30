fa6de9e8792952e4ce8356c6ace68b06095d4036
gongdananyou <gongdananyou@qq.com>
2020-06-30 22:15:37 +0800

    update

---
+++ js/L2Dwidget.init.js
@@ -0,0 +1,22 @@
+L2Dwidget.init({
+	"pluginRootPath": "https://cdn.jsdelivr.net/gh/gongdananyou/blogcdn@master/live2dw/",
+	"pluginJsPath": "lib/",
+	"pluginModelPath": "assets/",
+	"tagMode": false,
+	"debug": false,
+	"model": {
+		"jsonPath": "https://cdn.jsdelivr.net/gh/gongdananyou/blogcdn@master/live2dw/assets/koharu.model.json"
+	},
+	"display": {
+		"position": "left",
+		"width": 200,
+		"height": 400
+	},
+	"mobile": {
+		"show": false
+	},
+	"react": {
+		"opacity": 1
+	},
+	"log": false
+});
